36fad637f7947cae20392e301333638b300a347c
Marcos V <usmarcos@yahoo.com.br>
2022-11-23 22:14:52 -0300

    Criando a opção de quantidade de rodadas e adapatando o código para aceitar essas informações.

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -2,6 +2,7 @@ package menu;
 
 import metodos.Jogo;
 import texto.Tabuleiro;
+
 import java.util.Scanner;
 
 public class Menu {
@@ -13,11 +14,12 @@ public class Menu {
             continuar = false;
             System.out.println("1 - Novo jogo");
             System.out.println("2 - Melhor de três");
-            System.out.println("3 - Instruções de jogo");
-            System.out.println("4 - Posições");
-            System.out.println("5 - Sair");
+            System.out.println("3 - Selecionar quantidade de partidas");
+            System.out.println("4 - Instruções de jogo");
+            System.out.println("5 - Posições");
+            System.out.println("6 - Sair");
             opcao = new Scanner(System.in).nextInt();
-            if (opcao <= 0 || opcao > 4) {
+            if (opcao <= 0 || opcao > 6) {
                 System.out.println("Opção inválida. Escolha uma das opções.");
                 continuar = true;
             }
@@ -27,18 +29,21 @@ public class Menu {
                 System.out.println("Iniciando novo jogo.");
                 new Jogo().jogo();
                 break;
-                case 2:
+            case 2:
                 System.out.println("Iniciando novo jogo de melhor de três");
                 System.out.println("Esse modo de jogo consiste em 3 rodadas que defeniram um vencedor ou empate.\n");
                 new Jogo().melhorDeTres();
                 break;
             case 3:
-                new Tabuleiro().montaTabulerioExemplo();
+                new Jogo().quantidadePartidas();
                 break;
             case 4:
-                new Tabuleiro().posicoes();
+                new Tabuleiro().montaTabulerioExemplo();
                 break;
             case 5:
+                new Tabuleiro().posicoes();
+                break;
+            case 6:
                 System.out.println("Finalizando jogo.");
                 System.exit(1);
                 break;
--- src/metodos/Mensagens.java
+++ src/metodos/Mensagens.java
@@ -1,14 +1,14 @@
 package metodos;
 
 public class Mensagens {
-    final String SEPARADOR = "\n------------------";
+    final String SEPARADOR = "\n------------------\n";
     final String MARCACAO = "Jogador 1 será o X \nJogador 2 será o O";
     final String MELHOR_DE_TRES = "\nINICIANDO MELHOR DE TRES";
     final String PRIMEIRA_RODADA = "\nPRIMEIRA RODADA";
     final String SEGUNDA_RODADA = "\nSEGUNDA RODADA";
     final String TERCEIRA_RODADA = "\nTERCEIRA RODADA";
-    final String JOGADA_JOGADOR_UM = "• Jogador 1 é sua vez";
-    final String JOGADA_JOGADOR_DOIS = "• Jogador 2 é sua vez";
+    final String JOGADA_JOGADOR_UM = "• Jogador 1 é sua vez!";
+    final String JOGADA_JOGADOR_DOIS = "• Jogador 2 é sua vez!";
     final String POSICAO_LINHA = "Informe a linha: ";
     final String POSICAO_COLUNA = "Informe a coluna: ";
     final String MENSAGEM_ERRO = "Jogada inválida, informe a linha e coluna de 0 a 3, combinando os valores para formar a jogada.\n";
@@ -16,11 +16,18 @@ public class Mensagens {
     final String SEM_VENCEDORES = "\nVELHA, não há vencedores. Inicie uma nova partida.";
     final String VENCEDOR_X = "\nO jogador 1 venceu (X)";
     final String VENCEDOR_O = "\nO jogador 1 venceu (O)";
-    final String PONTOS_JOGADOR_UM = "\nO jogador 1 fez %d ponto(s).";
+    final String PONTOS_JOGADOR_UM = "O jogador 1 fez %d ponto(s).";
     final String PONTOS_JOGADOR_DOIS = "\nO jogador 2 fez %d ponto(s).";
     final String VELHA = "\nHouveram %d empate(s) (velha).";
     final String NAO_HOUVERAM_VENCEDORES = "\nNão houveram vencedores";
     final String VENCEDOR_UM = "O vencedor foi o jogador 1!";
     final String VENCEDOR_DOIS = "O vencedor foi o jogador 2!";
     final String RESULTADO = "\nRESULTADO";
+    final String QUANTIDADE_DE_PARTIDAS = "MODO DE QUANTIDADE DE PARTIDAS";
+    final String PERGUNTA_QUANTIDADE_PARTIDAS = "\nSelecione a quantidade de partidas a serem jogadas (deve ser mais que uma): ";
+    final String QUANTIDADE_PARTIDAS_INVALIDO = "\nValor inválido, informe uma quantidade de rodadas maior que 1 e menor ou igual a 100.";
+    final String RODADA = "\n%dª RODADA";
+
+
+
 }
